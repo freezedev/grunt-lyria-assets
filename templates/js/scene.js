5a5fc33d5bbe74de948c465b53a6e7890890476e
Johannes Stein <johannesstein@freeze-dev.com>
2013-10-12 01:14:36 +0200

    Updated scene.js template

--- templates/js/scene.js
+++ templates/js/scene.js
@@ -1,19 +1,34 @@
-define('%= namespace %>/scenelist', ['lyria/scene', 'lyria/template/engine'], function(Scene, TemplateEngine) {
-  var sceneList = {};
-  <% _.each(scenes, function(scene) { %>
-    sceneList['<%= scene.name %>'] = new Scene('<%= scene.name %>', <%= scene.deps %>, function() {
-      this.localization = <%= scene.localization %>;
-      this.template = this.template || {};
-      this.template.source = <%= scene.templateSource %>;
-      this.template.partials = <%= scene.templatePartials %>;
-      
-      var sceneFunc = <%= scene.sceneContent %>;
-      if (typeof sceneFunc === 'function') {
-        sceneFunc = sceneFunc.apply(this, arguments);
-      }
-      
-      return sceneFunc;
-    });
-  <% }); %>
-  return sceneList;
+define('<%= namespace %>/<%= name %>', ['lyria/scene', 'lyria/template/engine', 'lyria/localization'], function(Scene, TemplateEngine, Localization) {
+  
+  return function(param) {
+    for (sceneKey in param) {
+      var sceneValue = param[sceneKey];
+      Scene.requireAlways[sceneKey] = sceneValue;
+    }
+    
+    var sceneList = {};
+    <% _.each(scenes, function(scene) { %>
+      sceneList['<%= scene.name %>'] = new Scene('<%= scene.name %>', <%= scene.deps %>, function() {
+        var self = this;
+        
+        this.localization = new Localization(<%= scene.localization %>);
+        this.template = this.template || {};
+        this.template.partials = <%= scene.template.partials %>;
+        this.template.source = TemplateEngine.compile(<%= scene.template.source %>, {
+          helpers: self.template.helpers,
+          partials: self.template.partials
+        });
+        
+        (function() {
+          <%= scene.content %>
+        }).call(this);
+        
+      });
+    <% }); %>
+    return sceneList;
+  };
+  
 });
+<% if (hasSourceMap) {%>
+<%= sourceMapDest %>
+<% } %>
