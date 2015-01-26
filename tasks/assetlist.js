c69a5a28c3bc21f1cc4bfb6fd4dc0777ccf3c344
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-26 13:43:11 +0100

    Try to update asset task to be more compliant to the official grunt task plugin spec

--- tasks/assetlist.js
+++ tasks/assetlist.js
@@ -13,35 +13,47 @@ module.exports = function(grunt) {
 
     var assetObject = {};
     var assetSize = 0;
+    
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        var dirname = (filepath.split('/')[1].indexOf('.') > 0) ? 'root' : filepath.split('/')[1];
 
-    for (var i = 0, j = files.length; i < j; i++) {
+        if (options.ignores.indexOf(dirname) >= 0) {
+          return;
+        }
 
-      (function(fileObject) {
+        assetObject[dirname] = assetObject[dirname] || {};
+        stat = fs.statSync(file);
+        assetObject[dirname].files = assetObject[dirname].files || [];
+        assetObject[dirname].files.push({
+          name: file,
+          type: mime.lookup(file),
+          size: stat.size
+        });
 
-        var dest = fileObject.dest;
-
-        for ( k = 0, l = fileObject.src.length; k < l; k++) {
+        if (assetObject[dirname].totalSize) {
+          assetObject[dirname].totalSize += stat.size;
+        } else {
+          assetObject[dirname].totalSize = stat.size;
+        }
+        
+      });
+      
+      for ( k = 0, l = fileObject.src.length; k < l; k++) {
           (function(file) {
-            var dirname = (file.split('/')[1].indexOf('.') > 0) ? 'root' : file.split('/')[1];
-
-            if (options.ignores.indexOf(dirname) >= 0) {
-              return;
-            }
-
-            assetObject[dirname] = assetObject[dirname] || {};
-            stat = fs.statSync(file);
-            assetObject[dirname].files = assetObject[dirname].files || [];
-            assetObject[dirname].files.push({
-              name: file,
-              type: mime.lookup(file),
-              size: stat.size
-            });
-
-            if (assetObject[dirname].totalSize) {
-              assetObject[dirname].totalSize += stat.size;
-            } else {
-              assetObject[dirname].totalSize = stat.size;
-            }
+            
           })(fileObject.src[k]);
         }
 
@@ -57,10 +69,13 @@ module.exports = function(grunt) {
         }
 
         grunt.file.write(dest, 'define("' + options.namespace + '/' + options.name + '",' + JSON.stringify(assetObject) + ');');
+     
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
 
-      })(files[i]);
-
-    }
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
 
   });
 };
